6f966b2a054bbee79b8cac2f960183ab68f36901
DrakLulu <charlotte.madroux@gmail.com>
2018-12-03 10:51:11 +0100

    Update PMF.ino
    Mise à jour des valeurs envoyées par l'arduino.

--- PMF.ino
+++ PMF.ino
@@ -10,65 +10,54 @@ int peltier_level = map(power, 0, 99, 0, 255); //This is a value from 0 to 255 t
 
 //PI
 int commande = 0;
-int consigne = 18; 
-int erreur = 0; 
-int ecart; 
+int consigne = 18;
+int erreur = 0;
+int ecart;
 float kp = 0.3;
 int ki = 8;
 int P_x = 0;
-int I_x = 0; 
+int I_x = 0;
 
 void setup() {
-pinMode(A0, INPUT) ; 
-Serial.begin(9600); // ouverture connexion serie
-Serial.println("Tets DHT22 ----------------------");  //
-dht.begin(); 
+  pinMode(A0, INPUT) ;
+  Serial.begin(9600); // ouverture connexion serie
+  dht.begin();
 }
 
 void loop() {
-float h = dht.readHumidity();    // lecture humidité et affectation a une variable de type float
-float t = dht.readTemperature(); // lecture température et affectation a une variable de type float
+  float h = dht.readHumidity();    // lecture humidité et affectation a une variable
+  float t = dht.readTemperature(); // lecture température et affectation a une variable
 
-float U = analogRead(0)*(5.0/1024);
-float R = (10000.0/((5.0/U)-1.0));
-float temp = (1/(0.001096702 + 0.00024005*log(R) + 0.0000000593486*(pow(log(R),3))))-273.15; 
-Serial.print("Temperature sonde : ");
-Serial.println(temp);
+  float U = analogRead(0) * (5.0 / 1024);
+  float R = (10000.0 / ((5.0 / U) - 1.0));
+  float temp = (1 / (0.001096702 + 0.00024005 * log(R) + 0.0000000593486 * (pow(log(R), 3)))) - 273.15;
 
-// test si retour sonde dht2  ok ou pas ...
-if (isnan(t) || isnan(h)) {
-Serial.println("Erreur de lecture sonde dht22");
-} else {
-//affichage des donnees sur la console serie
-Serial.print("Humidite : " ); 
-Serial.print(h);
-Serial.print( " %\t ");
-Serial.print(" Temperature DHT22: " );
-Serial.print(t);
-Serial.println(" C");
 
-ecart = consigne - temp; 
-P_x = kp * ecart;
-commande = P_x + I_x;
-I_x = I_x + ki * ecart;
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Erreur de lecture sonde dht22");
+  } else {
 
-power = power + commande;
+    /*ecart = consigne - temp;
+    P_x = kp * ecart;
+    commande = P_x + I_x;
+    I_x = I_x + ki * ecart;
+    power = power + commande;*/
+      if (t > 18.0){
+        power = 99; // module peltier allume
+      } else {
+        power = 0; //moduel perltier eteind
+      }
 
-peltier_level = map(power, 0, 99, 0, 255);
+    peltier_level = map(power, 0, 99, 0, 255);
+    analogWrite(peltier, peltier_level); 
+  
+  if (power == 99) {
+      Serial.println(temp +","+ t +","+ h +","+ "1" );
+    } else {
+      Serial.println(temp +","+ t +","+ h +","+ "0");
+    }
 
-Serial.print("Power=");
-Serial.print(power);
-Serial.print(" PLevel=");
-Serial.println(peltier_level);
-analogWrite(peltier, peltier_level); //Write this new value out to the port
-
-if(power == 99){
-  Serial.println("Allume");
-} else {
-  Serial.println("eteind"); 
-}
-
-delay(1000); // attente de 1 seconde avant de refaire une mesure
-}
+    delay(1000); 
+  }
 
 }
