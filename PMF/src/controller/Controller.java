d1c8927900d5f0735e5760a572dc2b12cab6d904
Pedro <simaopedro.martinsmoura@viacesi.fr>
2018-12-04 15:03:21 +0100

    interface fonctionel
    recupere infos arduino et les affiche

--- PMF/src/controller/Controller.java
+++ PMF/src/controller/Controller.java
@@ -1,12 +1,29 @@
 package controller;
-import view.View;
 
-public class Controller {
+import arduino.Arduino;
+import model.readArduino;
+import view.Panel;
+import view.Window;
 
- 
-	public Controller(View view) {
-		
-        
+public class controller {
+	Window win = new Window();
+	readArduino AA = new readArduino(this);
+
+	public controller() throws InterruptedException {
+
+		Thread read = new Thread(new readArduino(this));
+		read.start();
+
 	}
+
+	
+	public void run(String tempExt, String tempInt, String humi, String onOff) {
+		win.pan.setTemperaturext(tempExt);
+		win.pan.setTemperature(tempInt);
+		win.pan.setHumidite(humi);
+		
+	}
+
+	
 	
 }
